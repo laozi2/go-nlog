639145761c3dbad47ecba2fcb7e24d6230dde7d0
xiaohe_xie <xiaohe_xie@intsig.net>
2019-05-16 18:30:43 +0800

    x

--- logger.go
+++ logger.go
@@ -18,38 +18,89 @@ type Logger struct {
 	Out       io.WriteCloser
 }
 
-func (l *Logger) Debug(msg string, args ...interface{}) {
+// Debug outputs message, Arguments are handled by fmt.Sprint
+func (l *Logger) Debug(obj ...interface{}) {
+	if l.Level >= DEBUG {
+		l.log(DEBUG, fmt.Sprint(obj...))
+	}
+}
+
+// Info outputs message, Arguments are handled by fmt.Sprint
+func (l *Logger) Info(obj ...interface{}) {
+	if l.Level >= INFO {
+		l.log(INFO, fmt.Sprint(obj...))
+	}
+}
+
+// Print outputs message, Arguments are handled by fmt.Sprint
+func (l *Logger) Print(obj ...interface{}) {
+	if l.Level != OFF {
+		l.log(INFO, fmt.Sprint(obj...))
+	}
+}
+
+// Warn outputs message, Arguments are handled by fmt.Sprint
+func (l *Logger) Warn(obj ...interface{}) {
+	if l.Level >= WARN {
+		l.log(WARN, fmt.Sprint(obj...))
+	}
+}
+
+// Error outputs message, Arguments are handled by fmt.Sprint
+func (l *Logger) Error(obj ...interface{}) {
+	if l.Level >= ERROR {
+		l.log(ERROR, fmt.Sprint(obj...))
+	}
+}
+
+// Panic outputs message, and followed by a call to panic() Arguments are handled by fmt.Sprint
+func (l *Logger) Panic(obj ...interface{}) {
+	if l.Level >= PANIC {
+		l.log(PANIC, fmt.Sprint(obj...))
+	}
+	panic(fmt.Sprint(obj...))
+}
+
+// Fatal outputs message and followed by a call to os.Exit(1), Arguments are handled by fmt.Sprint
+func (l *Logger) Fatal(obj ...interface{}) {
+	if l.Level >= FATAL {
+		l.log(FATAL, fmt.Sprint(obj...))
+	}
+	Exit(1)
+}
+
+func (l *Logger) Debugf(msg string, args ...interface{}) {
 	if l.Level >= DEBUG {
 		l.log(DEBUG, fmt.Sprintf(msg, args...))
 	}
 }
 
-func (l *Logger) Info(msg string, args ...interface{}) {
+func (l *Logger) Infof(msg string, args ...interface{}) {
 	if l.Level >= INFO {
 		l.log(INFO, fmt.Sprintf(msg, args...))
 	}
 }
 
-func (l *Logger) Warn(msg string, args ...interface{}) {
+func (l *Logger) Warnf(msg string, args ...interface{}) {
 	if l.Level >= WARN {
 		l.log(WARN, fmt.Sprintf(msg, args...))
 	}
 }
 
-func (l *Logger) Error(msg string, args ...interface{}) {
+func (l *Logger) Errorf(msg string, args ...interface{}) {
 	if l.Level >= ERROR {
 		l.log(ERROR, fmt.Sprintf(msg, args...))
 	}
 }
 
-func (l *Logger) Panic(msg string, args ...interface{}) {
+func (l *Logger) Panicf(msg string, args ...interface{}) {
 	if l.Level >= PANIC {
 		l.log(PANIC, fmt.Sprintf(msg, args...))
 	}
 	panic(fmt.Sprintf(msg, args...))
 }
 
-func (l *Logger) Fatal(msg string, args ...interface{}) {
+func (l *Logger) Fatalf(msg string, args ...interface{}) {
 	if l.Level >= FATAL {
 		l.log(FATAL, fmt.Sprintf(msg, args...))
 	}
