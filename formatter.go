f8f3db738700a63f6c4f0fad035459b32ffa92a6
xiaohe_xie <xiaohe_xie@intsig.net>
2019-05-16 17:22:27 +0800

    change file&line

--- formatter.go
+++ formatter.go
@@ -8,7 +8,7 @@ import (
 	"regexp"
 	"runtime"
 	"strconv"
-	"strings"
+	//	"strings"
 	"sync"
 	"time"
 )
@@ -126,28 +126,16 @@ func formatterMsg(buf *bytes.Buffer, msg string) {
 
 // FilelineCaller returns file and line for caller
 func FilelineCaller(skip int) (file string, line int) {
-	for i := 0; i < 10; i++ {
-		_, file, line, ok := runtime.Caller(skip + i)
-		if !ok {
-			return "???", 0
-		}
-
-		// file = pkg/file.go
-		n := 0
-		for i := len(file) - 1; i > 0; i-- {
-			if file[i] == '/' {
-				n++
-				if n >= 2 {
-					file = file[i+1:]
-					break
-				}
-			}
-		}
-
-		if !strings.HasPrefix(file, "go-log/") {
-			return file, line
-		}
+	_, file, line, ok := runtime.Caller(skip)
+	if !ok {
+		return "???", 0
 	}
 
-	return "???", 0
+	for i := len(file) - 1; i > 0; i-- {
+		if file[i] == '/' {
+			file = file[i+1:]
+			break
+		}
+	}
+	return file, line
 }
